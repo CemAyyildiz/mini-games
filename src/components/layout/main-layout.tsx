f338f96e2cd8887c59933c3702173b52fe5a4a77
CemAyyildiz <ayyldz1925@gmail.com>
2025-12-08 17:23:56 +0300

    fix: remove Navbar component and adjust main padding in MainLayout

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from './sidebar';
-import Navbar from './navbar';
 
 export default function MainLayout() {
   return (
     <div className="min-h-screen bg-background">
       <Sidebar />
       <div className="pl-[280px]">
-        <Navbar />
-        <main className="pt-16">
+        <main className="pt-6">
           <div className="container mx-auto p-6">
             <Outlet />
           </div>
